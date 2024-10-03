a814613cd544f402427fed5f991e34d6d64d7fdf
yogeshnew5exy <149184955+yogeshnew5exy@users.noreply.github.com>
2024-10-03 18:26:11 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -96,14 +96,9 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "▰" * completed_length + "▱" * remaining_length
 
             # Choose sticker based on progress
-            sticker = "alexwa_by_fStikBot"  # Replace this with the actual sticker ID
-            if completed_length == bar_length:
-                sticker = "alexwa_by_fStikBot"  # Final sticker when upload is complete
-            elif completed_length > 0:
-                sticker = "alexwa_by_fStikBot"  # Sticker for in-progress
             
             try:
-                await reply.edit(f'╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰────⌈ 🇨‌ 🇴‌ 🇻 🇮 🇩 ⌋────╯\n') 
+                await reply.edit(f'╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰────⌈ 🇾 🇴 🇬 🇪 🇸 🇭 ⌋────╯\n') 
          #       await reply.edit(f'╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰─⌈  𝘽𝙤𝙩 𝙈𝙖𝙙𝙚 𝙗𝙮 🇨‌ 🇴‌ 🇻 🇮 🇩 ⌋─╯\n') 
             except FloodWait as e:
                 time.sleep(e.x)
